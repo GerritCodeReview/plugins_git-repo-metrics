fdd7ed8676127c3ce8b8549215962879f948c087
Fabio Ponciroli <ponch78@gmail.com>
2022-08-22 15:35:20 +0200

    Dynamically load stats collectors
    Allows dynamic loading of stats collectors.
    This groundwork will allow to easily add new stats collectors
    to gather more metrics and informations about the
    git repositories.
    
    Added MetricsCollector interface to define collectors API.
    
    Bug: Issue 16208
    
    Change-Id: Iabd18153b5faf43cb91044a9a02fcc7b7efda23f

---
+++ src/test/java/com/googlesource/gerrit/plugins/gitrepometrics/FakeMetricsCollector.java
@@ -0,0 +1,53 @@
+// Copyright (C) 2022 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.gitrepometrics;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import com.google.gerrit.entities.Project;
+import com.googlesource.gerrit.plugins.gitrepometrics.collectors.GitRepoMetric;
+import com.googlesource.gerrit.plugins.gitrepometrics.collectors.MetricsCollector;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import org.eclipse.jgit.internal.storage.file.FileRepository;
+import org.junit.Ignore;
+
+@Ignore
+public class FakeMetricsCollector implements MetricsCollector {
+  @Override
+  public HashMap<String, Long> collect() {
+    return Maps.newHashMap(ImmutableMap.of("fake-metrics-1", 1L, "fake-metrics-2", 2L));
+  }
+
+  @Override
+  public MetricsCollector create(FileRepository repository, Project project) {
+    return new FakeMetricsCollector();
+  }
+
+  @Override
+  public String getStatName() {
+    return "fake-metrics-collector";
+  }
+
+  @Override
+  public List<GitRepoMetric> availableMetrics() {
+    return Arrays.asList(
+        new GitRepoMetric("fake-metric-1", "Fake metric 1", "Count"),
+        new GitRepoMetric("fake-metric-2", "Fake metric 2", "Count"));
+  }
+
+  protected FakeMetricsCollector() {}
+}
