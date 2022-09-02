799b0311ed7d366862b5f384615b33f932949817
Fabio Ponciroli <ponch78@gmail.com>
2022-09-02 18:09:14 +0200

    Don't initialize metrics at startup
    Start populating metrics only when collected. Avoid defaulting
    them all to 0 at startup.
    
    Bug: Issue 16236
    Change-Id: I76712db1819c4b8a2fbc15f4b660c357ad42c7dc

--- src/test/java/com/googlesource/gerrit/plugins/gitrepometrics/FakeMetricsCollector.java
+++ src/test/java/com/googlesource/gerrit/plugins/gitrepometrics/FakeMetricsCollector.java
@@ -26,10 +26,9 @@ import org.junit.Ignore;
 
 @Ignore
 public class FakeMetricsCollector implements MetricsCollector {
-  public static final GitRepoMetric fakeMetric1 =
-      new GitRepoMetric("fake-metric-1", "Fake metric 1", "Count");
-  public static final GitRepoMetric fakeMetric2 =
-      new GitRepoMetric("fake-metric-2", "Fake metric 2", "Count");
+  private final String prefix;
+  private final GitRepoMetric fakeMetric1;
+  private final GitRepoMetric fakeMetric2;
 
   @Override
   public HashMap<GitRepoMetric, Long> collect(FileRepository repository, Project project) {
@@ -38,7 +37,7 @@ public class FakeMetricsCollector implements MetricsCollector {
 
   @Override
   public String getMetricsCollectorName() {
-    return "fake-metrics-collector";
+    return prefix + "-fake-metrics-collector";
   }
 
   @Override
@@ -46,5 +45,13 @@ public class FakeMetricsCollector implements MetricsCollector {
     return ImmutableList.of(fakeMetric1, fakeMetric2);
   }
 
-  protected FakeMetricsCollector() {}
+  protected FakeMetricsCollector(String prefix) {
+    this.prefix = prefix;
+    this.fakeMetric1 = new GitRepoMetric(prefix + "-fake-metric-1", "Fake metric 1", "Count");
+    this.fakeMetric2 = new GitRepoMetric(prefix + "-fake-metric-2", "Fake metric 2", "Count");
+  }
+
+  protected FakeMetricsCollector() {
+    this("defaultPrefix");
+  }
 }
