ebc1552d877027d2b37c59581c5bcc2c17b63936
Luca Milanesio <luca.milanesio@gmail.com>
2024-05-22 15:06:34 +0100

    Allow to force metrics update on repositories
    Define a new configuration git-repo-metrics.forcedCollection for
    forcing the recalculation of the projects metrics even in absence
    of git-ref-update events.
    
    The executor service type is changed to ScheduledExecutor for being
    used to schedule metrics collection at regular intervals.
    
    Change-Id: Ia5d7d59e2262fb30a46308ecc91b86a17dbb92b1

--- src/main/java/com/googlesource/gerrit/plugins/gitrepometrics/Module.java
+++ src/main/java/com/googlesource/gerrit/plugins/gitrepometrics/Module.java
@@ -17,6 +17,7 @@ package com.googlesource.gerrit.plugins.gitrepometrics;
 import com.google.gerrit.extensions.registration.DynamicSet;
 import com.google.gerrit.lifecycle.LifecycleModule;
 import com.google.gerrit.server.events.EventListener;
+import com.google.inject.Inject;
 import com.google.inject.Scopes;
 import com.google.inject.name.Names;
 import com.googlesource.gerrit.plugins.gitrepometrics.collectors.FSMetricsCollector;
@@ -24,18 +25,29 @@ import com.googlesource.gerrit.plugins.gitrepometrics.collectors.GitRefsMetricsC
 import com.googlesource.gerrit.plugins.gitrepometrics.collectors.GitStatsMetricsCollector;
 import com.googlesource.gerrit.plugins.gitrepometrics.collectors.MetricsCollector;
 import com.googlesource.gerrit.plugins.gitrepometrics.collectors.NumberOfProjectsCollector;
-import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ScheduledExecutorService;
 
 public class Module extends LifecycleModule {
+
+  private final GitRepoMetricsConfig config;
+
+  @Inject
+  Module(GitRepoMetricsConfig config) {
+    this.config = config;
+  }
 
   @Override
   protected void configure() {
     bind(GitRepoMetricsCache.class).in(Scopes.SINGLETON);
-    bind(ExecutorService.class)
+    bind(ScheduledExecutorService.class)
         .annotatedWith(UpdateGitMetricsExecutor.class)
         .toProvider(UpdateGitMetricsExecutorProvider.class);
     bind(GitRepoUpdateListener.class);
     DynamicSet.bind(binder(), EventListener.class).to(GitRepoUpdateListener.class);
+
+    if (config.isForcedCollection()) {
+      listener().to(GitRepoMetricsScheduler.class);
+    }
 
     DynamicSet.setOf(binder(), MetricsCollector.class);
     DynamicSet.bind(binder(), MetricsCollector.class).to(GitStatsMetricsCollector.class);
