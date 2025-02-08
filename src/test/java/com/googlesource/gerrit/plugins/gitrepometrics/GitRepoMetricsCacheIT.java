c377b9814a844fa97e0b15bc59f07c40e1888f45
Daniele Sassoli <danielesassoli@gmail.com>
2025-02-08 15:24:47 +0000

    Add the ability to access different SCM repositories
    For now, this is limited to GitLab, the intention is to extend this
    to other Git SCMs too.
    
    GitLab stores repositories under a different path from Gerrit, some
    logic is required to calculate the new path.
    When using GitLab backend, the project should be set to the GitLab Id
    of that project, rather than the name. The metric will be exposed with
    the project Id too. Further details on GitLab project ids at [1].
    
    [1]https://docs.gitlab.com/ee/user/project/working_with_projects.html#access-a-project-by-using-the-project-id
    
    Change-Id: Ic5fa65b49dbab255a2ede9c3e5d4f017c219bf18

--- src/test/java/com/googlesource/gerrit/plugins/gitrepometrics/GitRepoMetricsCacheIT.java
+++ src/test/java/com/googlesource/gerrit/plugins/gitrepometrics/GitRepoMetricsCacheIT.java
@@ -27,6 +27,7 @@ import com.google.inject.Inject;
 import com.googlesource.gerrit.plugins.gitrepometrics.collectors.FSMetricsCollector;
 import com.googlesource.gerrit.plugins.gitrepometrics.collectors.GitRefsMetricsCollector;
 import com.googlesource.gerrit.plugins.gitrepometrics.collectors.GitStatsMetricsCollector;
+import java.io.IOException;
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
@@ -66,10 +67,22 @@ public class GitRepoMetricsCacheIT extends LightweightPluginDaemonTest {
       pluginName = "git-repo-metrics",
       name = "git-repo-metrics.project",
       values = {"testProject1", "testProject2"})
-  public void shouldRegisterAllMetrics() {
+  public void shouldRegisterAllMetrics() throws IOException {
+    ConfigSetupUtils configSetupUtils =
+        new ConfigSetupUtils(Arrays.asList("testProject1", "testProject2"));
     List<Project.NameKey> availableProjects = Arrays.asList(testProject1, testProject2);
-    new UpdateGitMetricsTask(gitRepoMetricsCache, repoManager, testProject1.get()).run();
-    new UpdateGitMetricsTask(gitRepoMetricsCache, repoManager, testProject2.get()).run();
+    new UpdateGitMetricsTask(
+            gitRepoMetricsCache,
+            repoManager,
+            configSetupUtils.getGitRepoMetricsConfig(),
+            testProject1.get())
+        .run();
+    new UpdateGitMetricsTask(
+            gitRepoMetricsCache,
+            repoManager,
+            configSetupUtils.getGitRepoMetricsConfig(),
+            testProject2.get())
+        .run();
 
     int expectedMetricsCount =
         fsMetricsCollector.availableMetrics().size()
