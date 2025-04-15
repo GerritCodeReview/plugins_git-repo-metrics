b17be9e6792c31b2dc877a016134c2f72158633a
Fabio Ponciroli <ponch78@gmail.com>
2025-04-15 18:18:48 +0200

    Avoid dropping stats collection tasks
    The current back-pressure mechanism for stats collection incorrectly
    drops tasks.
    
    Instead of discarding tasks, the logic should compact multiple pending
    tasks into a single one, reducing pressure on the filesystem while still
    ensuring data is collected.
    
    Bug: Issue 410584024
    Change-Id: Ie82379e33041c67ee41932e62c0facf04a3b43ae

--- src/test/java/com/googlesource/gerrit/plugins/gitrepometrics/GitRepoMetricsCacheTest.java
+++ src/test/java/com/googlesource/gerrit/plugins/gitrepometrics/GitRepoMetricsCacheTest.java
@@ -23,9 +23,6 @@ import com.google.gerrit.extensions.registration.DynamicSet;
 import com.googlesource.gerrit.plugins.gitrepometrics.collectors.GitRepoMetric;
 import com.googlesource.gerrit.plugins.gitrepometrics.collectors.MetricsCollector;
 import java.io.IOException;
-import java.time.Clock;
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -124,50 +121,16 @@ public class GitRepoMetricsCacheTest {
   }
 
   @Test
-  public void shouldSkipCollectionWhenGracePeriodIsNotExpired() throws IOException {
-    ConfigSetupUtils configSetupUtils =
-        new ConfigSetupUtils(Collections.singletonList(enabledRepo), "5 m");
-    gitRepoMetricsConfig = configSetupUtils.getGitRepoMetricsConfig();
-    gitRepoMetricsCache =
-        new GitRepoMetricsCache(ds, fakeMetricMaker, new MetricRegistry(), gitRepoMetricsConfig);
-
-    gitRepoMetricsCache.setMetrics(getCollectedMetrics(), enabledRepo);
-
-    assertThat(gitRepoMetricsCache.shouldCollectStats(enabledRepo)).isFalse();
-  }
-
-  @Test
   public void shouldCollectStatsWhenGracePeriodIsExpired() throws IOException {
     ConfigSetupUtils configSetupUtils =
         new ConfigSetupUtils(Collections.singletonList(enabledRepo), "1 s");
     gitRepoMetricsConfig = configSetupUtils.getGitRepoMetricsConfig();
     gitRepoMetricsCache =
-        new GitRepoMetricsCache(
-            ds,
-            fakeMetricMaker,
-            metricRegistry,
-            gitRepoMetricsConfig,
-            Clock.fixed(
-                Instant.now().minus(2, ChronoUnit.SECONDS), Clock.systemDefaultZone().getZone()));
+        new GitRepoMetricsCache(ds, fakeMetricMaker, metricRegistry, gitRepoMetricsConfig);
 
     gitRepoMetricsCache.setMetrics(getCollectedMetrics(), enabledRepo);
 
     assertThat(gitRepoMetricsCache.shouldCollectStats(enabledRepo)).isTrue();
-  }
-
-  @Test
-  public void shouldSetCollectionTime() throws IOException {
-    ConfigSetupUtils configSetupUtils =
-        new ConfigSetupUtils(Collections.singletonList(enabledRepo));
-    gitRepoMetricsConfig = configSetupUtils.getGitRepoMetricsConfig();
-    gitRepoMetricsCache =
-        new GitRepoMetricsCache(ds, fakeMetricMaker, metricRegistry, gitRepoMetricsConfig);
-
-    long currentTimeStamp = System.currentTimeMillis();
-
-    gitRepoMetricsCache.setMetrics(getCollectedMetrics(), enabledRepo);
-
-    assertThat(gitRepoMetricsCache.getCollectedAt().get(enabledRepo)).isAtLeast(currentTimeStamp);
   }
 
   private HashMap<GitRepoMetric, Long> getCollectedMetrics() {
